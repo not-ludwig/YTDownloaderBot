ea10a1d54214de19491411f7a194095a36da6fec
md5-loki <lokillo0012@gmail.com>
2021-05-21 20:57:46 -0400

    comments

--- main.py
+++ main.py
@@ -1,48 +1,50 @@
+# -- Imports -- #
+
 from pytube import YouTube
 from telegram.ext import Updater, CommandHandler, ConversationHandler, MessageHandler, Filters
 import logging
 import os
 import time
-
-# The correct way to import a token stored in .env file
 from dotenv import load_dotenv
-load_dotenv() 
-#It configures your local enviromental variable and merge to the enviromental variable tree
 
+# -- The correct way to import a token stored in .env file -- #
+# -- It configures your local enviromental variable and merge to the enviromental variable tree -- #
+load_dotenv() 
 token = os.getenv('token')
 
+# -- Basic Setup -- #
 updater = Updater(token, use_context=True)
 dispatcher = updater.dispatcher
 LINK = 0
-
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
+# -- Main Commands -- #
 def start(update, context):
     context.bot.send_message(chat_id = update.effective_chat.id, text = "I'm a bot, say something")
 
 def get_link(update, context):
     context.bot.send_message(chat_id = update.effective_chat.id, text = "Send me the link you wish to download! ")
-    return LINK
+    # -- Hold value to use in Conversation Handler -- #
+    return LINK 
 
 def download(update, context):
+    # -- Get video info -- #
     video = YouTube(update.message.text)
     audio = video.streams.filter(only_audio=True).first()
     title = video.title.translate(str.maketrans('','',".,'"))
-   
+    
+    # -- Download audio stream and change its extension --  #
     pre = audio.download()
-    
     post = os.path.splitext(pre)[0]
-
-    
     os.rename(pre, post + '.mp3')
 
     context.bot.send_message(chat_id = update.effective_chat.id, text = 'Wait...') 
     context.bot.send_audio(chat_id = update.effective_chat.id, audio = open(title + '.mp3', 'rb'))
     
+    # -- Wait 5 seconds then delete the video file -- #
     time.sleep(5)
-
     os.remove(title + '.mp3')
-
+    # -- End the conversation handler since input is not expected or alredy given -- #
     return ConversationHandler.END
 
 start_handler = CommandHandler('start', start)
